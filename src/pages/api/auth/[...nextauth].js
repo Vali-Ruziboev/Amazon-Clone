b18db88e84622d35f018e8de26e1f74e864934f2
Vali <valiruziboyev212@gmail.com>
2022-06-30 16:37:14 +0500

    jwt error fix

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,5 +1,17 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
+import { getToken } from "next-auth/jwt"
+
+const secret = process.env.NEXTAUTH_SECRET
+
+export async function handler(req, res) {
+  // if using `NEXTAUTH_SECRET` env variable, we detect it, and you won't actually need to `secret`
+  // const token = await getToken({ req })
+  const token = await getToken({ req, secret })
+  console.log("JSON Web Token", token)
+  res.end()
+}
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
