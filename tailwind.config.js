19c009142122e2901d5441051a83db84be60d23a
Vali <valiruziboyev212@gmail.com>
2022-07-08 17:02:52 +0500

    config change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
 module.exports = {
   mode: "jit",
-  purge: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
